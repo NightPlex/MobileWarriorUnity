c99d7471b40a18a2c735e7448ed4ecfc0faf4d05
NightPlex <provensoul@gmail.com>
2018-11-20 23:17:34 +0200

    Initial commit

---
+++ Assets/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	private Animator _animatorPlayer;
+	
+	[SerializeField]
+	private Animator _animatorSwordArc;
+	
+	void Start () {
+		_animatorPlayer = GetComponentInChildren<Animator>();
+		//_animatorSwordArc = transform.GetChild(1).GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public void Move (float move) {
+		_animatorPlayer.SetFloat("Move", Mathf.Abs(move));
+	}
+
+	public void Jump() {
+		_animatorPlayer.SetTrigger("triggerJump");
+	}
+
+	public void Attack() {
+		_animatorPlayer.SetTrigger("triggerAttack");
+		_animatorSwordArc.SetTrigger("SwordAnimation");
+	}
+}
